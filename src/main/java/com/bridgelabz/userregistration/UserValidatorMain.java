30bea59267ddbb96fae93c749bf89b0c7f6e6e65
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-12 19:24:19 +0530

    Added:initial setup for user registration using gradle.

---
+++ src/main/java/com/bridgelabz/userregistration/UserValidatorMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.userregistration;
+public class UserValidatorMain
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/test/java/com/bridgelabz/userregistration/JunitUserRegistrationTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.userregistration;
+import org.junit.jupiter.api.Test;
+
+public class JunitUserRegistrationTest
+{
+   @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue()
+    {
+
+    }
+}
